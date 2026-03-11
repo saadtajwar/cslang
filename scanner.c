2fdd8cb34c8f65704a5f2b079ce23987b6e4d27f
Saad Tajwar <saads@ad.unc.edu>
2026-03-11 19:51:50 -0400

    indent

--- scanner.c
+++ scanner.c
@@ -37,17 +37,17 @@ Token scanToken() {
         case '/': return makeToken(TOKEN_SLASH);
         case '*': return makeToken(TOKEN_STAR);
         case '!':
-        return makeToken(
-            match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+            return makeToken(
+                match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
         case '=':
-        return makeToken(
-            match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+            return makeToken(
+                match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
         case '<':
-        return makeToken(
-            match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+            return makeToken(
+                match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
         case '>':
-        return makeToken(
-            match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+            return makeToken(
+                match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
     }
     
     return errorToken("Unexpected character.");
